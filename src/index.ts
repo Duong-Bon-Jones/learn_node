af09e9a1e295d046b0ac7aa9f072b94bef4d222c
duong <iamtdthewinner@gmail.com>
2025-09-02 11:32:51 +0700

    move back to BE only

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import genresRoute from "./routes/genres.js";
-import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono();
 
 const appRoutes = app.basePath("/api").route("/genres", genresRoute);
 
 export type AppType = typeof appRoutes;
-
-app.get("*", serveStatic({ root: "./client/.output/public" }));
-app.get("*", serveStatic({ path: "./client/.output/public/_shell.html" }));
 
 serve(
   {
